0c2be155be56fe557c814977e3d72461daec96d2
coder14-cloud <vanshita986@gmail.com>
2024-08-04 18:14:53 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,12 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Hey, This Healthmate Chatbot" />
 </template>
+<br>
+<p>Click in the right bottom to begin your health checkup</p>
+<br>
+<p>Made By Vanshita Sabnani</p>
 
 <style scoped>
 .logo {
